64f9a8155f0c7ccc798975ca3a3e31045a8c5039
fllsouto <fllsouto@gmail.com>
2013-09-21 19:10:43 -0300

    todos choram

--- ep1/filtro.pl
+++ ep1/filtro.pl
@@ -5,11 +5,38 @@ use warnings;
 
 my @string = split(' ', <>);# nao precisa do aspas
 chomp (@string);
-my @string2 = @string;
-
+my @string2;
 for (my $a = 0; $a < scalar @string; $a++) {
 	if(not ($string[$a] =~ /^-[1-9]{1,3}/)){
-		print "$string[$a] \n";
+		my $item = $string[$a];
+		push (@string2,$item);
+
 	}
+
+}
+
+# for (my $a = 0; $a < scalar @string2; $a++) {
+# 	print "$string2[$a] \n";
+# }
+
+readInput(@string2);
+
+
+sub readInput{
+    my @string = @_;
+    my $a = 0;
+
+    for (my $a = 0; $a < scalar @string - 1; $a++) {
+    	my $waka = $string[$a] % 9;
+    	if ($waka != 0) {
+    		print "$waka  ";
+    	} else {
+    		print "9  ";
+    	}
+    	if ((($a+1) % 9) == 0){
+    		print "\n\n";
+    	}
+
+    }
 
 }
